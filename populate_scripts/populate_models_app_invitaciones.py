b12f699f90055a964053464dcef731f82cd9d381
jaha_devcrack <jaha.devcrack@gmail.com>
2019-05-08 16:04:21 -0500

    Se agregaron notas acerca del funcionamiento DB

--- populate_scripts/populate_models_app_invitaciones.py
+++ populate_scripts/populate_models_app_invitaciones.py
@@ -23,6 +23,12 @@ def add_Invitaciones(N=10):
 
     Attributes:
 
+    Todo:
+        * Se tiene que obtener la empresa con la que se tiene que vincular, la invitacion.
+        * Obtener el area vinculada con la empresa y la invitacion.
+        * Obtenemos el empleado vinculado con la empresa que es quien genera la invitacion.
+        * Generamos el usuario al que le esta asiganda la invitacion.
+        * Finalmente se genera la invitacion.
     """
 
     a_length = len(Empresa.objects.all())
@@ -75,6 +81,20 @@ def add_Invitaciones(N=10):
 
 
 def add_InvitacionTemporal(N=10):
+    """Se agrega un determinado numero de registros a la tabla InvitacionTemporal.
+
+        Args:
+            N(int):Por default son 10 registro pero en realidad puede tomar un valor que le sea proporcionado.
+
+        Attributes:
+
+        Todo:
+            * Se tiene que obtener la empresa con la que se tiene que vincular, la invitacion.
+            * Obtener el area vinculada con la empresa y la invitacion.
+            * Obtener el empleado vinculado con la empresa que es quien genera la invitacion.
+            * Generamos el usuario al que le esta asiganda la invitacion.
+            * Finalmente se genera la invitacion.
+        """
     a_length = len(Empresa.objects.all())
     if a_length > 1:
         # Obtenemos un registro de la tabla empresa para vincularla con las invitaciones a generar.
