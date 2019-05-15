63da8e6d408b90f3b3f0b4dd001b0c504846d1ac
Jesús Alejandro Lara Moreno <jesus_alexlm@alumnos.uaslp.edu.mx>
2019-05-15 11:01:51 -0500

    Se elimino el perfil y se agrego a la tabla inicial de usuario

--- Usuarios/models.py
+++ Usuarios/models.py
@@ -9,22 +9,30 @@ class CustomUser(AbstractUser):
     Mas información:
         https://docs.djangoproject.com/en/2.2/topics/auth/customizing/#extending-django-s-default-user
     """
-    user_perfil = models.ForeignKey('Perfil', on_delete=models.CASCADE, related_name='perfil_user_perfil')
+    #user_perfil = models.ForeignKey('Perfil', on_delete=models.CASCADE, related_name='perfil_user_perfil')
 
+    """
+    Perfil
+    """
+    celular = models.CharField(max_length=30, unique=True, null=False, blank=True, name='celular')
+    ine_frente = models.CharField(max_length=25, null=False, blank=True, name='ine_frente')
+    ine_atras = models.CharField(max_length=25, null=False, blank=True, name='ine_atras')
+    roll = models.IntegerField(null=False, default=0, blank=True, name='roll')
+    plataforma = models.CharField(max_length=25, default='', name='plataforma')  # Tipo de aplicacion que es el sistema(Web, Android, iOs)
     """
     Para que no haya problemas al momento de crear un nuevo usuario se crea su perfil y se le asigna :D.
     Esto se logra sobreescribiendo el método de Save..
     Este metodo solo es para que funcione createsuperuser, con DRF se crea primero el modelo de perfil y se le asigna
     al usuario todo estó mediante la vista.
     """
-    def save(self, *args, **kwargs):
-
-        if not hasattr(self, 'user_perfil'):
-            p = Perfil(celular=str(self.username)+' - no number',
-                       ine_frente='na', ine_atras='na', es_empleado=False, plataforma='na')
-            p.save()
-            self.user_perfil = p
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #
+    #     if not hasattr(self, 'user_perfil'):
+    #         p = Perfil(celular=str(self.username)+' - no number',
+    #                    ine_frente='na', ine_atras='na', es_empleado=False, plataforma='na')
+    #         p.save()
+    #         self.user_perfil = p
+    #     super().save(*args, **kwargs)
 
 
 class Perfil(models.Model):
