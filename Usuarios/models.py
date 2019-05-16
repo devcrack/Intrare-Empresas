6800a1b8ba8ce164b5e1a2529997d5d7c77ee704
jaha_devcrack <jaha.devcrack@gmail.com>
2019-05-16 13:05:50 -0500

    Notas

--- Usuarios/models.py
+++ Usuarios/models.py
@@ -17,7 +17,7 @@ class CustomUser(AbstractUser):
     celular = models.CharField(max_length=30, unique=True, null=False, blank=True, name='celular')
     ine_frente = models.CharField(max_length=25, null=False, blank=True, name='ine_frente')
     ine_atras = models.CharField(max_length=25, null=False, blank=True, name='ine_atras')
-    roll = models.IntegerField(null=False, default=0, blank=True, name='roll')
+    roll = models.IntegerField(null=False, default=0, blank=True, name='roll') #DOCUMENTAR ESTO
     plataforma = models.CharField(max_length=25, default='', name='plataforma')  # Tipo de aplicacion que es el sistema(Web, Android, iOs)
     """
     Para que no haya problemas al momento de crear un nuevo usuario se crea su perfil y se le asigna :D.
