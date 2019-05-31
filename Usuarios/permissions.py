e0f8d8413979b1cf59865ed49a7cbd9342ba2c9d
Andrés <andes204@gmail.com>
2019-05-31 13:53:24 -0500

    Se corrió el error con los permisos

--- Usuarios/permissions.py
+++ Usuarios/permissions.py
@@ -5,6 +5,42 @@ class isSuperAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_superuser)
 
+class isAdminUserOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print("Primer Nivel")
+        return bool(request.user and request.user.roll == settings.ADMIN)
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Método que filtra los permisos de acuerdo al Administrador
+        de la Empresa.
+        :param request:
+        :param view:
+        :param obj:
+        :return: True: Si tiene los permisos
+                 False: Si no tiene los permisos
+        """
+        print("Segundo Nivel")
+        return request.user == obj.custom_user
+
+class isAdminUserOwnerArea(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print("Primer Nivel")
+        return bool(request.user and request.user.roll == settings.ADMIN)
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Método que filtra los permisos de acuerdo al Administrador
+        de la Empresa.
+        :param request:
+        :param view:
+        :param obj:
+        :return: True: Si tiene los permisos
+                 False: Si no tiene los permisos
+        """
+        print("Segundo Nivel")
+        return request.user == obj.id_empresa.custom_user
+
 
 class isEmployee(permissions.BasePermission):
     def has_permission(self, request, view):
