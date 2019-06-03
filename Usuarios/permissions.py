cfe683f2f2914f5d91793009dbeee9ebf08b1aca
jaha_devcrack <jaha.devcrack@gmail.com>
2019-06-03 13:09:56 -0500

    I dont know wtf is happening

--- Usuarios/permissions.py
+++ Usuarios/permissions.py
@@ -1,9 +1,52 @@
 from rest_framework import permissions
 from django.conf import settings
+from Usuarios.models import CustomUser
+from Empresas.models import Area
 
 class isSuperAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_superuser)
+
+
+class isAdminUserOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print("Primer Nivel")
+        print(request.user and request.user.is_superuser)
+        return bool(request.user and request.user.is_superuser)
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Método que filtra los permisos de acuerdo al Administrador
+        de la Empresa.
+        :param request:
+        :param view:
+        :param obj:
+        :return: True: Si tiene los permisos
+                 False: Si no tiene los permisos
+        """
+        print("Segundo Nivel")
+        return request.user == obj.custom_user
+
+
+class isAdminUserOwnerArea(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print("Primer Nivel")
+        print(request.user and request.user.is_superuser)
+        return bool(request.user and request.user.is_superuser)
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Método que filtra los permisos de acuerdo al Administrador
+        de la Empresa.
+        :param request:
+        :param view:
+        :param obj:
+        :return: True: Si tiene los permisos
+                 False: Si no tiene los permisos
+        """
+        print("Segundo Nivel")
+        return request.user == obj.id_empresa.custom_user
+
 
 class isEmployee(permissions.BasePermission):
     def has_permission(self, request, view):
