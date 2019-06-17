e94db3c71500132ab25fd3db8890db9a92a2c9a9
jaha_devcrack <jaha.devcrack@gmail.com>
2019-06-17 14:47:48 -0500

    struggle with create invitation endpoint

--- Invitaciones/serializers.py
+++ Invitaciones/serializers.py
@@ -4,23 +4,47 @@ from .models import *
 from Empresas.models import Administrador,Empresa
 from Empresas.models import Empleado
 
+class json_invit():
+
+    def __init__(
+            self, cell_number,
+            email, area,
+            inv_date, business,
+            sec_equip, vehicle,
+            notes, company_from):
+        self.cell_number = cell_number
+        self.email = email,
+        self.area = area,
+        self.inv_date = inv_date,
+        self.business = business,
+        self.sec_equip = sec_equip,
+        self.vehicle = vehicle,
+        self.notes = notes,
+        self.company_from = company_from
+
+    # def __init__(self, email, content, created):
+    #     self.email = email
+    #     self.content = content
+    #     self.created = created
+
 
 class InvitacionCreateSerializer(serializers.Serializer):
-    # cell_number = serializers.IntegerField()
-    # email = serializers.EmailField()
-    # area = serializers.CharField()
-    # inv_date = serializers.DateField()
-    # business = serializers.CharField()
-    # sec_equip = serializers.CharField()
-    # vehicle = serializers.BooleanField()
-    # notes = serializers.CharField()
-    # company_from = serializers.CharField()
+    cell_number = serializers.IntegerField()
     email = serializers.EmailField()
-    content = serializers.CharField(max_length=200)
-    created = serializers.DateTimeField()
+    area = serializers.CharField()
+    inv_date = serializers.DateField()
+    business = serializers.CharField()
+    sec_equip = serializers.CharField()
+    vehicle = serializers.BooleanField()
+    notes = serializers.CharField()
+    company_from = serializers.CharField()
+    # email = serializers.EmailField()
+    # content = serializers.CharField(max_length=200)
+    # created = serializers.DateTimeField()
 
     def create(self, validated_data):
         print('HELLO FROM INVITATION_CREATE_SERIALIZER fuckeeeeerr!!!!!\n')
+        return json_invit(**validated_data)
 
 
 
--- Invitaciones/views.py
+++ Invitaciones/views.py
@@ -1,3 +1,99 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework import viewsets
+from datetime import date
+from rest_framework.response import Response
+from .serializers import *
+from Usuarios.permissions import *
+from rest_framework import status
+
+class Invitacion_List(viewsets.ModelViewSet):
+    permission_classes = (IsAdmin | IsEmployee,)  # The user logged have to be and admin or an employee
+    serializer_class = InvitacionSerializers  # Used for validate and deserializing input, and for serializing output.
+
+    def list(self, request, *args, **kwargs):
+        usr = self.request.user
+        invitations = None
+        if usr.roll == settings.ADMIN:  # Admin must be show all invitations of  the Company.
+            print('IS an ADMINISTRATOR')
+            adm_company = Administrador.objects.filter(id_usuario=usr)[0]
+            id_company = adm_company.id_empresa
+            invitations = Invitacion.objects.filter(id_empresa=id_company)
+        if usr.roll == settings.EMPLEADO:
+            print('IS an EMPLOYEE')
+            employee = Empleado.objects.filter(id_usuario=usr)[0]
+            invitations = Invitacion.objects.filter(id_empleado=employee.id)
+        queryset = self.queryset = invitations
+        print(queryset.values_list())
+        print('\n')
+        print(type(queryset))
+        serializer = InvitacionSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+
+class InvitationCreate(generics.CreateAPIView):
+    permission_classes = (IsAdmin | IsEmployee,)
+    serializer_class = InvitacionCreateSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+
+        Args:
+
+        Notes:
+            For Empleado user is necesary this data:
+                Telefono:
+                Correo Electronico:
+                Area:
+                Fecha y hora de la invitacion
+                Asunto:
+                Vehiculo:
+                Notas:
+                Empresa:
+        todo:
+            * Validar que las areas en el JASON existan.
+            * Auto cargar el Id del empleado en el JSON a serializar.
+            * Si el Administrador genera la invitacion Validar que existe el empleado al que se asigna en la invitacion.
+            * Auto generar el tipo de invitacion de acuerdo a los datos del invitado:
+                Es invitacion, o invitacion temporal
+
+        """
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            usr = self.request.user
+            tmp_data = request.data.copy()
+            print('SERIALIZER DATA\n')
+            print(type(serializer.data))
+
+            #self.serializer_class()
+            if usr.roll == settings.ADMIN:  # Admin must be show all invitations of  the Company.
+                adm_company = Administrador.objects.filter(id_usuario=usr)[0]
+                id_company = adm_company.id_empresa
+                print('Administrador\n')
+            if usr.roll == settings.EMPLEADO:
+                employee = Empleado.objects.filter(id_usuario=usr)[0]
+                id_company = employee.id_empresa
+
+
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+
+            #print(tmp_data)
+
+            #number_phone = tmp_data['celular']  # .____.
+
+            #print('Celular', number_phone)  # We have determine if a user exists with this Number
+            #user = CustomUser.objects.filter(celular=number_phone)
+            #if user:  # If user exist
+             #   user = user[0]
+                ##self.serializer_class = InvitacionSerializers
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# How the fuck documment p
+# https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+
