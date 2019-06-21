71ad938c4811f4206d23821da19859cd538fed59
yo <jaha_devcrack@gmail.com>
2019-06-21 08:27:31 -0500

    i want to finish this

--- Invitaciones/views.py
+++ Invitaciones/views.py
@@ -60,7 +60,7 @@ class InvitationCreate(generics.CreateAPIView):
         print('ROLL FROM VIEW', usr.roll)
         #self.serializer_class()
         if usr.roll == settings.ADMIN:  # Admin must be show all invitations of  the Company.
-            print('Administrator\n')
+            print('ROLL->Administrator\n')
             adm_company = Administrador.objects.filter(id_usuario=usr)[0]
             id_company = adm_company.id_empresa
             print('Current Company->', id_company, '\n')
@@ -69,64 +69,32 @@ class InvitationCreate(generics.CreateAPIView):
             serializer = self.serializer_class(data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                employee_first_name = serializer.data['employee_first_name']  # First name employee provided
-                employee_last_name = serializer.data['employee_last_name']  # Last name employee provided
+                _first_name = serializer.data['employee_first_name']  # First name employee provided
+                _last_name = serializer.data['employee_last_name']  # Last name employee provided
                 area_name = serializer.data['area']
                 sec_equip_name = serializer.data['sec_equip']
 
                 """
                 Company Data Validations
                 """
-                area_s = Area.objects.filter(id_empresa=id_company, nombre=area_name)
-                if area_s:  # VALIDATE AREAS
-                    area = area_s[0]
-
-                    sec_equips = EquipoSeguridad.objects.filter(nombre=sec_equip_name)
-
-                    if sec_equips:#Validate
-
-                    print('Current AREA: ', area.nombre)
-                    employee_usr_s = CustomUser.objects.filter(first_name=employee_first_name, last_name=employee_last_name)
-                    if employee_usr_s:  # USER EXIST
-                        _usr_s = employee_usr_s[0]
-                        print('Current User:', _usr_s.id, '\n')
-
-                        # Trying get employee in current logged company with current user obtained.
-                        employee_s = Empleado.objects.filter(id_empresa=id_company, id_usuario=_usr_s.id)
-
-                        if employee_s:  # EMPLOYEE EXIST
-                            get_employee = employee_s[0]
-                            print('Employee FOUND! goal!! Goal!!! SUCCESS!!!!')
-                            # self.create_invitation(
-                            #     serializer.data,
-                            #     id_company,
-                            #     )
-                        else:
-                            print('Employee NOT FOUND')
-                            error_response = {'Error': 'The Employee provided do not exist'}
+                error_response, area = self.validate_areas(id_company, area_name)
+                if area:  # Validate if AREA Exist
+                    if sec_equip_name:  # Validate if security equipment Exist.
+                        error_response, security_equipment = self.validate_security_equip(sec_equip_name)
+                        if error_response:
                             return Response(data=error_response, status=status.HTTP_404_NOT_FOUND)
+                    error_response, employee = self.validate_employee(id_company, _first_name, _last_name)
+                    if employee:  # Validate if EMPLOYEE exist
+                        """"
+                        Now we can proceed to create an Invitation 
+                        """
+                        print('<SUCCESS>!!!!!!!!!!!!!!!!')
+
                     else:
-                        error_response = {'Error': 'User with provided full name do not exist'}
                         return Response(data=error_response, status=status.HTTP_404_NOT_FOUND)
                 else:
-                    error_response = {'Error': 'The Area provided do not exist'}
                     return Response(data=error_response, status=status.HTTP_404_NOT_FOUND)
-
-                """
-                Guest data validation
-                """
-                if self.guest_exist(serializer.data):  # Create a normal invitation.
-                    print('USER EXIST')
-                    # self.create_invitation(
-                    #     serializer.data,
-                    #     id_company,
-                    #     None,
-                    #     usr
-                    # )
-                else:
-                    print('USER dont EXIST')
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_204_NO_CONTENT)
         if usr.roll == settings.EMPLEADO:
             print('Employee\n')
             employee = Empleado.objects.filter(id_usuario=usr)[0]
@@ -182,6 +150,77 @@ class InvitationCreate(generics.CreateAPIView):
         #     id_usuario=user,
         #
         #     )
+
+    @classmethod
+    def validate_employee(cls, *args):
+        """
+        Args:
+            args[0]: Id_company.
+            args[1]: first_name.
+            args[2]: last_name.
+
+        Returns:
+            tuple:data error message and area if is found it.
+        """
+        id_company = args[0]
+        _first_name = args[1]
+        _last_name = args[2]
+        error_response = None
+        employee = None
+        _usr_s = CustomUser.objects.filter(first_name=_first_name, last_name=_last_name)
+        if _usr_s:
+            usr = _usr_s[0]
+            print('Current USER: ', usr.id)
+            employee_s = Empleado.objects.filter(id_empresa=id_company, id_usuario=usr.id)
+            if employee_s:
+                print('Employee FOUND! goal!! Goal!!! SUCCESS!!!!')
+                employee = employee_s[0]
+            else:
+                print('Employee NOT FOUND')
+                error_response = {'Error': 'The Employee provided do not exist'}
+        else:
+            print('USER Not Found')
+            error_response = {'Error': 'User with provided full name do not exist'}
+
+        return error_response, employee
+
+    @classmethod
+    def validate_areas(cls,*args):
+        """
+            Args:
+                args[0]: Id_company.
+                args[1]: area name.
+
+            Returns:
+                tuple:data error message and area if is found it.
+        """
+
+        id_company = args[0]
+        area_name = args[1]
+        area = None
+        error_response = None
+
+        area_s = Area.objects.filter(id_empresa=id_company, nombre=area_name)
+        if area_s:
+            area = area_s[0]
+            print('Area FOUND: ', area.nombre)
+        else:
+            error_response = {'Error': 'NO areas found with data provided'}
+        return error_response, area
+
+    @classmethod
+    def validate_security_equip(cls, name_security_equipment):
+        security_equipment = None
+        error_response = None
+        st_equipments = EquipoSeguridad.objects.filter(nombre=name_security_equipment)
+        if st_equipments:
+            security_equipment = st_equipments[0]
+        else:
+            error_response = {'Error': 'No Security Equipment found with data provided'}
+        return error_response, security_equipment
+
+
+
 # How the fuck document p
 # https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
 
