34bca5f8078bed6e966f991ad0c4d7c6bcc110a0
yo <jaha_devcrack@gmail.com>
2019-06-25 16:26:46 -0500

    Merge Andriu changes

--- Usuarios/serializers.py
+++ Usuarios/serializers.py
@@ -1,6 +1,14 @@
 from djoser.serializers import UserSerializer as BaseUserSerializer
+from rest_framework import serializers
+from .models import *
 
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields = ('email', 'username', 'roll', 'first_name', 'last_name', 'celular', 'is_staff', 'is_superuser')
+
+
+class UserAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
