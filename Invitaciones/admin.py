e00bb25594bf217cc4f6097d806555b90d6c0155
yo <jaha_devcrack@gmail.com>
2019-08-08 16:55:31 -0500

    MOdificado admin de invitaciones visualiza cosas diferentes en django admin

--- Invitaciones/admin.py
+++ Invitaciones/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 
-from .models import Invitacion, InvitacionTemporal, InvitacionEmpresarial
-from .models import EquipoSeguridad, EquiposporInvitacion, EquipoporInvitacionesEmpresariales
-from .models import EquipoporInvitacionTemporal
-
+from .models import EquipoSeguridad, EquiposporInvitacion
+from .models import Invitacion
 admin.site.register(Invitacion)
-admin.site.register(InvitacionTemporal)
-admin.site.register(InvitacionEmpresarial)
 admin.site.register(EquipoSeguridad)
 admin.site.register(EquiposporInvitacion)
-admin.site.register(EquipoporInvitacionesEmpresariales)
-admin.site.register(EquipoporInvitacionTemporal)
