3037f2ac291113b05e3b5163b7a846bbfce35012
jaha_devcrack <jaha_devcrack@gmail.com>
2019-08-24 05:49:35 -0500

    Vista para creacion de usuario plataforma, Hay que modificar los permisos

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,3 +1,13 @@
+from rest_framework import generics
+
+from .serializers import *
+from .permissions import *
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class UserPlatformCreateOrList(generics.CreateAPIView):
+    permission_classes = (isSuperAdmin | isEmployee,)
+    queryset = CustomUser.objects.all()
+    serializer_class = UserPlatformSerializer
