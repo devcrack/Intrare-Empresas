547c42ca9d2f61f1780a5f3f4e861b6a0bed06f1
Andrés <andes204@gmail.com>
2019-08-27 20:52:40 -0500

    Se agregó el campo id al Serializador UserSerializer

--- Usuarios/serializers.py
+++ Usuarios/serializers.py
@@ -11,7 +11,7 @@ from .models import *
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
-        fields = ('email', 'username', 'roll', 'first_name', 'last_name', 'celular', 'is_staff', 'is_superuser')
+        fields = ('id', 'email', 'username', 'roll', 'first_name', 'last_name', 'celular', 'is_staff', 'is_superuser')
 
 
 class UserAdminSerializer(serializers.ModelSerializer):
