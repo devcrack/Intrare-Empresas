d3924b554b989031d387c7fbea2f170d7c216fd3
jaha_devcrack <jaha_devcrack@gmail.com>
2019-09-04 19:32:10 -0500

    Actualizando Usuario

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('UserPlatformCreate/', UserPlatformCreateOrList.as_view(), name='CreateUserPlatform'),
+    path('UserPlatformUpdate/', UserUpdateParcial.as_view(), name='UpdateUserPlatform'),
 ]
--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
-
+from rest_framework.permissions import IsAuthenticated
 from .serializers import *
 from .permissions import *
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,3 +11,19 @@ from django.shortcuts import render
 class UserPlatformCreateOrList(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserPlatformSerializer
+
+
+class UserUpdateParcial(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        instance = self.request.user
+        # user = self.request.user
+
+        print('prev mail =', instance.email)
+        instance.email = request.data.get('email')
+        print('post mail =', instance.email)
+        instance.save()
+        print(type(instance))
+
+
