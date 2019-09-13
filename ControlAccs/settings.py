21e5b03d5e46fb360a4ee6c5925c3b99283e9d8a
Andrés <andes204@gmail.com>
2019-09-12 19:40:13 -0500

    Update

--- ControlAccs/settings.py
+++ ControlAccs/settings.py
@@ -37,11 +37,11 @@ AUTH_USER_MODEL = 'Usuarios.CustomUser'
 
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework.authentication.TokenAuthentication',
-    #     #'rest_framework.authentication.BasicAuthentication',
-    #     #'rest_framework.authentication.SessionAuthentication',
-    # ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
