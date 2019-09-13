14e3a08cc2ce5c69ceccbe71ea65a01f62445fc8
jaha_devcrack <jaha_devcrack@gmail.com>
2019-09-13 01:55:59 -0500

    Se dejo señalado las 2 proximas urls y vistas a generarse para manejar usuarios con preregristro y ya registrados

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -5,6 +5,8 @@ from .views import *
 urlpatterns = [
     path('users/filter', UserViewSet.as_view({'get': 'list'})),
     path('UserPlatformCreate/', UserPlatformCreateOrList.as_view(), name='CreateUserPlatform'),
+    # path('UserPlatformUpdateLogged/', UserUpdateParcial.as_view()),
+    # path('UserPlatformUpdateNoLogged/', UserUpdateParcial.as_view()),
     path('UserPlatformUpdate/', UserUpdateParcial.as_view(), name='UpdateUserPlatform'),
     path('UserPasswordUpdate/', UserPasswordUpdate.as_view(), name='UpdateUserPlatform'),
     path('UserImgUpdate/', UserImgUpdate.as_view(), name='UserImagesUpdate'),
