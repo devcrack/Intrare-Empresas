9bf94363b4d66447a24f30c1044d7ff744648a5c
jaha_devcrack <jaha_devcrack@gmail.com>
2019-09-16 19:19:22 -0500

    corregido patch user?

--- Usuarios/serializers.py
+++ Usuarios/serializers.py
@@ -53,7 +53,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
         send_IntrareEmail(html_message, instance.email)
         query_set = Invitacion.objects.filter(id_usuario=instance)[0]
         if len(query_set) > 0:  # Si el usuario tiene invitacion enviar Email con invitacion 0.
-            inv = Invitacion.objects.filter(id_usuario=instance)[0]
+            inv = query_set[0]
             html_message = render_to_string('email.html',
                                             {'empresa': inv.id_empresa.name,
                                              'fecha': inv.fecha_hora_invitacion,
