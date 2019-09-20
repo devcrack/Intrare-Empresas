66d0a049d79a3de0b069f0a1b98a74ae60cdf305
Aurelio Hernandez <jaha.devcrack@gmail.com>
2019-09-19 19:15:21 -0500

    Agregado timeDelta a la fecha default de expiracion

--- Invitaciones/models.py
+++ Invitaciones/models.py
@@ -22,8 +22,11 @@ class Invitacion(models.Model):
     typeInv = models.IntegerField(default=0, null=False)  # 0=Inv Normal, 1=Recurrente 2= Referidos
     dateInv = models.DateField(default=date(year=timezone.now().year, month=timezone.now().month, day=timezone.now().day+1), null = False)
     timeInv = models.TimeField(default=time(), null=False)
-    expiration = models.DateField(default=date(year=timezone.now().year, month=timezone.now().month, day=timezone.now().day+2), null=False)
-    diary = models.CharField(max_length=7, default="")  # Dias de la semana que asistira recurrentemente
+    _DateExp = dateInv.get_default()
+    _delta = timezone.timedelta(days=2)
+    _DateExp = _DateExp + _delta
+    expiration = models.DateField(default=_DateExp, null=False)
+    diary = models.CharField(max_length=7, default="")  # Dias de la semana que asistira recurrentemente LMXJVSD
     #
     asunto = models.CharField(max_length=254, null=False, blank=False)
     automovil = models.BooleanField(null=False, blank=False)
