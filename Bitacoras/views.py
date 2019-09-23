fae1d483be0c90ad5cee0fc4473eb79f2b0453a9
andes204 <andes204@gmail.com>
2019-09-23 10:58:24 -0500

    Se agregaron 2 endpoints, para listar detalle de accesos y firmar pase

--- Bitacoras/views.py
+++ Bitacoras/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
-from rest_framework import generics, status
+from rest_framework import generics, status, viewsets
 from Usuarios.permissions import *
 from .serializers import *
-from Empresas.models import Vigilante
+from Empresas.models import Vigilante, Administrador
 
 
 class BitacoraListCreate(generics.ListCreateAPIView):
@@ -22,3 +22,44 @@ class BitacoraListCreate(generics.ListCreateAPIView):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
+
+class BitacoraUpdate(generics.UpdateAPIView):
+    permission_classes = (isAdmin | isGuard, )
+    queryset = Bitacora.objects.all()
+    lookup_field = 'pk'
+    serializer_class = BitacoraSerializers
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        request.data['f_salida'] = datetime.now()
+        return self.update(request, *args, **kwargs)
+
+
+class BitacoraListGuard(viewsets.ModelViewSet):
+    permission_classes = (isAdmin | isGuard, )
+    serializer_class = BitacoraListGuardSerializers
+
+    def list(self, request, *args, **kwargs):
+        y = self.kwargs['year']
+        m = self.kwargs['month']
+        d = self.kwargs['day']
+        usr = self.request.user
+        registros = None
+        if usr.roll == settings.ADMIN:  # Admin must be show all records of the Company.
+            print('IS an ADMIN')
+            admin_company = Administrador.objects.filter(id_usuario=usr)[0]
+            id_company = admin_company.id_empresa
+            registros = Bitacora.objects.filter(id_empresa=id_company, f_acceso__year=y,
+                                                f_acceso__month=m,
+                                                f_acceso__day=d)
+        if usr.roll == settings.VIGILANTE:  # Guard must be show all records of the Company.
+            print('IS an GUARD')
+            guard_company = Vigilante.objects.filter(id_usuario=usr)[0]
+            id_company = guard_company.id_empresa
+            registros = Bitacora.objects.filter(id_empresa=id_company, f_acceso__year=y,
+                                                f_acceso__month=m,
+                                                f_acceso__day=d)
+
+        queryset = self.queryset = registros
+        serializer = BitacoraListGuardSerializers(queryset, many=True, context={"request": request})
+        return Response(serializer.data)
