54511f07849a67be42aae2bab3142435c0db3974
jaha_devcrack <jaha.devcrack@gmail.com>
2019-09-24 17:34:19 -0500

    Corregido Error Serilizador

--- Invitaciones/serializers.py
+++ Invitaciones/serializers.py
@@ -6,6 +6,39 @@ from Empresas.models import Administrador, Empresa, Area
 from Empresas.models import Empleado
 
 
+class InvitacionSerializers(serializers.ModelSerializer):
+    """ Serializer just for invitations.
+
+    Class Evidently extended from ModelSerializer.
+    The ModelSerializer class provides a shortcut that lets you automatically create a Serializer class
+    with fields that correspond to the Model fields.
+
+    The ModelSerializer class is the same as a regular Serializer class, except that:
+    It will automatically generate a set of fields for you, based on the model.
+    It will automatically generate validators for the serializer, such as unique_together validators.
+    It includes simple default implementations of .create() and .update().
+    """
+    #id_usuario = serializers.SerializerMethodField('get_user')
+
+
+    class Meta:
+        model = Invitacion  # Desired Model to be serialized.
+        #fields = '__all__'  # Indicates that all fields in model should be used.
+        fields = (
+            'id_empresa',
+            'id_area',
+            'id_empleado',
+            'id_usuario',
+            'fecha_hora_envio',
+            'dateInv',
+            'timeInv',
+            'asunto',
+            'automovil',
+            'notas',
+            'empresa',
+            'leida'
+        )
+
 # def validateDateInv(value):
 #     _date = date(year=timezone.now().year, month=timezone.now().month, day=timezone.now().day)
 #     if _date > value:
