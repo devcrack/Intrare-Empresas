626a4693340d22a8617325cf67e8b25a795b996f
jaha_devcrack <jaha.devcrack@gmail.com>
2019-10-02 20:12:30 -0500

    Agregado icono

--- Invitaciones/views.py
+++ Invitaciones/views.py
@@ -115,7 +115,7 @@ def createOneMoreInvitaitons(id_company, id_area, _host, listGuest, typeInv, _da
         if _idUser.is_active:  # El proceso de notificacion de Invitacion se realiza normalmente
             try:
                 device = FCMDevice.objects.get(user=_idUser)
-                ico = "/media/intrare.ico"
+                _icon = 'https://bucketeer-576c8228-7737-4878-8397-1c8403d07005.s3.amazonaws.com/images/asdfadf/intrare.ico'
 
                 host_name = _host.first_name + _host.last_name
             except ObjectDoesNotExist:
@@ -123,7 +123,7 @@ def createOneMoreInvitaitons(id_company, id_area, _host, listGuest, typeInv, _da
             device.send_message(
                 title="Intrare",
                 body="Has Recibido una Invitación de parte de: " + host_name,
-                icon=ico)
+                icon=_icon)
             _msgInv = "Se te ha enviado una invitacion, verifica desde tu correo electrónico o en la aplicacion"
             _dateTime = str(inv.dateInv) + " " + str(inv.timeInv)
             _htmlMessage = render_InvMail(inv.id_empresa.name, _dateTime,
