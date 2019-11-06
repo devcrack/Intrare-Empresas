03d4b351471c92d1c2c3e1774a7118e2ad31fc0d
jaha_devcrack <jaha.devcrack@gmail.com>
2019-11-06 16:05:35 -0600

    no se que hice

--- Invitaciones/views.py
+++ Invitaciones/views.py
@@ -25,7 +25,6 @@ _regexMail = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
 linkWallet = 'https://api-intrare-empresarial.herokuapp.com/wallet/create/'  # Production V1
 #linkPreregisterUser = 'https://web-intrare.herokuapp.com/preregistro/'  # Development
 linkPreregisterUser = 'https://first-project-vuejs.herokuapp.com/preregistro/'  # Production V1
-linkConfirmAppointment = "url"
 linkConfirmAppointment = "https://api-intrare-empresarial.herokuapp.com/setConfirmed_Appointment/" #Production V1
 
 
