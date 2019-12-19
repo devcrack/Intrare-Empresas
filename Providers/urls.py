db1f27a4813ad9cc6791df037a08698f45de99f0
Aurelio <aurelio.hdz.aguilar@gmail.com>
2019-12-19 17:44:18 -0600

    Corregido campos

--- Providers/urls.py
+++ Providers/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('getProvider/<temporalToken>/', getProviderByToken.as_view()),
     path('updateProvider/<temporalToken>/', updatePartialProvider.as_view()),
+    path('createCompanyProvider', AddCompanyProvider.as_view()),
     ]
