99fc22d84d0703691d0e47eb5764d685dc545dc9
Aurelio <aurelio.hdz.aguilar@gmail.com>
2020-01-13 17:51:14 -0600

    Obtener accesos sin  invitacion por rango de fechas

--- Bitacoras/urls.py
+++ Bitacoras/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('bitacora/', BitacoraListCreate.as_view(), name='bitacora_list_create'),
     path('get_bitacora/<year>/<month>/<day>/', BitacoraListGuard.as_view({'get': 'list'}), name='get_invitations_url'),
-    path('get_bitacoraByDateRange/<year1>/<month1>/<day1>/<year2>/<month2>/<day2>/',BitacoraListToGuardByDateRange({'get': 'list'})),
+    path('get_bitacoraByDateRange/<year1>/<month1>/<day1>/<year2>/<month2>/<day2>/',
+         BitacoraListToGuardByDateRange.as_view({'get': 'list'})),
     path('firmar_salida/<int:pk>/', BitacoraUpdate.as_view(), name='firma_pase_bitacora'),
 ]
